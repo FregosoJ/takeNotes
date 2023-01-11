470f03d19205d7f871911de11b0186242b6ed03c
Jonathan Fregoso <jonathanfregoso@Jonathans-MBP.lan>
2023-01-10 19:43:05 -0800

    made new folder for takenotes, and uploaded files from noteTaker

---
+++ routes/notes.js
@@ -0,0 +1,113 @@
+const notes = require('express').Router();
+const fs = require("fs");
+const util = require("util");
+const readFileAsync = util.promisify(fs.readFile);
+const uuid =()=> {
+    return Math.floor(Math.random()* 1000000)
+};
+const { response } = require('.');
+
+
+// GET request for API (/api/notes)
+notes.get("/notes", (req, res) => {
+    console.log("here");
+
+    // log our request to terminal
+    console.info(`${req.method} request received to get notes`);
+
+    // need to read db.json file and return all saved notes as JSON
+    readFileAsync(`db/db.json`, "utf8").then((data) => {
+        // convert string to JSON object
+        let noteArr = JSON.parse(data);
+        console.log(noteArr);
+        return res.json(noteArr);
+    });
+});
+
+
+// POST request for API (/api/notes)
+notes.post("/notes", (req, res) => {
+    console.log("you made it");
+    // log that POST request was received
+    console.info(`${req.method} request received to add notes`);
+    console.log(req.body);
+    // destructure assignment for items in req.body
+    const { title, text } = req.body;
+
+    // if both properties are present
+    if (title && text) {
+        console.log("if statement");
+        // variable for object we want to save
+        const newNote = {
+            title,
+            text,
+            id: uuid(), // ~ installed npm uuid package
+        };
+        console.log(newNote);
+        // obtain existing notes
+        fs.readFile("db/db.json", "utf8", (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log("data:", data);
+                // convert string to JSON object
+                let noteArr = JSON.parse(data);
+                // add new note to array
+                noteArr.push(newNote);
+                console.log("note array:", noteArr);
+                // (stringify array) write string to a file
+                fs.writeFile(`db/db.json`, JSON.stringify(noteArr, null, "\t"), (err) =>
+                    err
+                        ? console.error(err)
+                        : console.log(
+                            `Note for ${newNote.noteTitle} has been written to JSON file`
+                        )
+                );
+            }
+        });
+
+        const response = {
+            status: "success",
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json("Error in posting note");
+    }
+});
+
+// *BONUS*
+// delete clicked note
+notes.delete(`/notes/:id`, (req, res) => {
+    // add logic to figure out how to remove from the array where the notes id matches the value of that parameter ~ filter functional loop
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+
+        if (err) {
+            console.error(err);
+        } else {
+            // convert string to JSON object
+            let noteArr = JSON.parse(data);
+            // note = noteArr[i] (each individual item in array)
+            const deletedArr = noteArr.filter(function (note) {
+                if (note.id != req.params.id) {
+                    console.log("message")
+                    return note;
+
+                }
+            });
+
+
+            // (stringify array) write string to a file
+            fs.writeFile(`db/db.json`, JSON.stringify(deletedArr, null, "\t"), (err) =>
+                err
+                    ? console.error(err)
+                    : res.json(deletedArr)
+            );
+        }
+    });
+
+})
+
+module.exports = notes;
---
+++ server.js
@@ -0,0 +1,42 @@
+// require 
+const express = require("express");
+const path = require("path");
+const api = require('./routes/notes');
+
+// Port
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+// set static folder (essentially telling express that anything static comes from the public folder)
+app.use(express.static("public"));
+
+// GET /notes ~ should return the notes.html file
+app.get("/notes", (req, res) => {
+    // send message to client
+    // res.json(`${req.method} request received to get notes html file`);
+    // send file
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    // log request to terminal
+    console.info(`${req.method} request received to get notes html file`);
+});
+
+// GET * (wildcard route) ~ should return the index.html file
+app.get('*', (req, res) => {
+    // send message to client
+    // res.json(`${req.method} request received to get index html file`); // ?? this feels wrong
+    // send file
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    // log request to terminal
+    console.info(`${req.method} request received to get index html file`);
+})
+
+// at the bottom - listener ~ app.listen(PORT, () =>)
+app.listen(PORT, () =>
+    console.log(`App is listening on http://localhost:${PORT}`)
+);
